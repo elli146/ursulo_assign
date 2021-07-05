39a78d60651702ee0ef6fa5725c9616bd29adb7c
elli146 <86464826+elli146@users.noreply.github.com>
2021-07-05 21:20:50 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php 
+ echo "<center>"."Dariela Ursulo"."</center>"."<br/>";
+ echo "<center>"."ACT-2"."</center>"."<br/>";
+
+//constant 
+define ('Message','Constant');
+echo "<br/>";
+echo Message;
+echo "<br/>";
+define('integer',9);
+echo integer;
+echo "<br/>";
+
+
+//array
+$array1 =['white','bw','orange'];
+$array2 =array('white','bw','orange');
+echo $array1[0];
+echo "<br/>";
+echo $array2[2];
+
+$num1= 100;
+$num2 = 100;
+echo "<center>"."First number: $num1"."</center>"."<br>";
+echo "<center>"."Second number: $num2"."</center>"."<br>";
+
+ // if empty($user) = TRUE, set $status = "anonymous"
+   echo $status = (empty($user)) ? "anonymous" : "logged in";
+   echo("<br>");
+
+   $user = "John Doe";
+   // if empty($user) = FALSE, set $status = "logged in"
+   echo $status = (empty($user)) ? "anonymous" : "logged in";
+   
+ echo("<br>");
+//Function 
+function showmessage ($message){
+	return "Name: $message";
+}
+echo showmessage("Dariela Ursulo");
+
+echo "<br/>";
+$g = 27;
+
+if ($g >=1 && $g <=10)
+echo "y";
+elseif ($g >=11 && $g <= 20)
+echo ("n");
+else 
+{
+	echo "false";
+}
+echo "<br/>";
+//switch 
+
+switch ($g){
+	case 1: 
+	echo"n"; break;
+	case 27: 
+	echo"x"; break;
+	case 28: 
+	echo"i"; break;
+	default: 
+	echo "wrong input";
+	}
